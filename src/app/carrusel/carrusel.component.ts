84353c3ce685d79c46790e2064856c7564e42ad3
Israel Borja <iborja@vc.com.mx>
2018-11-23 13:35:05 -0600

    Se genera esqueleto

---
+++ src/app/carrusel/carrusel.component.ts
@@ -0,0 +1,41 @@
+
+import { Component, OnInit } from '@angular/core';
+declare var jquery:any;
+declare var $ :any;
+
+@Component({
+  selector: 'app-carrusel',
+  templateUrl: './carrusel.component.html',
+  styleUrls: ['./carrusel.component.css']
+})
+export class CarruselComponent implements OnInit {
+
+  constructor() { }
+  slides = [
+    {img: "http://placehold.it/350x150/000000"},
+    {img: "http://placehold.it/350x150/111111"},
+    {img: "http://placehold.it/350x150/333333"},
+    {img: "http://placehold.it/350x150/666666"}
+  ];
+  slideConfig = {"slidesToShow": 1, "slidesToScroll": 1, "dots": true,
+  //"autoplay": true,
+  //"autoplaySpeed": 2000,
+  "arrows": true};
+
+  addSlide() {
+    this.slides.push({img: "http://placehold.it/350x150/777777"})
+  }
+
+  removeSlide() {
+    this.slides.length = this.slides.length - 1;
+  }
+
+  afterChange(e) {
+    console.log('afterChange');
+  }
+  ngOnInit() {
+   
+  }
+  }
+
+
